0d6df6cd96b9557957bfa4c3ac5774b6fc9a71e0
Your Name <szmania@yahoo.com>
2026-03-07 14:37:24 -0800

    fix: Restore summaryManager and update letter parsing logic

--- src/main/ts/conversation_interfaces.ts
+++ src/main/ts/conversation_interfaces.ts
@@ -32,7 +32,8 @@ export interface Setting {
 
 export interface Summary{
     date: string,
-    content: string
+    content: string,
+    characterId?: string;
 }
 
 export interface ActionArgument{
