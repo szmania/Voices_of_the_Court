ccb5b2e5716c7197251a1cd27c5e1df0903ca7d1
Your Name <szmania@yahoo.com>
2026-03-11 23:11:27 -0700

    fix: Set short volume name for macOS DMG build in forge config

--- forge.config.js
+++ forge.config.js
@@ -21,7 +21,8 @@ module.exports = {
     {
       name: '@electron-forge/maker-dmg',
       config: {
-        format: 'ULFO'
+        format: 'ULFO',
+        name: 'VOTC-CE'
       }
     },
     {
